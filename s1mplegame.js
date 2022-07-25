3d0b60b9e98a783dd0ef0ec3c14b034c8632ac83
hoang4963 <hoangth.finbox@gmail.com>
2022-07-25 09:46:53 +0700

    a

--- s1mplegame.js
+++ s1mplegame.js
@@ -14,30 +14,37 @@ function Hero(image, top, left, size , speed){
 
     this.moveRight = function(){
         this.left += speed;
-        console.log('ok: ' + this.left);
+        console.log('ok: ' + this.left + '  ' + this.top );
     }
-    this.setSpeed = function (speed) {
+    this.setSpeed = function(speed) {
         this.speed = speed;
     }
-    this.moveLeft = function () {
+    this.moveLeft = function() {
         this.left -= speed;
+        console.log('ok: ' + this.left + '  ' + this.top );
     }
-    this.moveDown = function () {
+    this.moveDown = function() {
         this.top += speed;
+        console.log('ok: ' + this.left + '  ' + this.top );
     }
-    this.moveUp = function () {
+    this.moveUp = function() {
         this.top -= speed;
+        console.log('ok: ' + this.left + '  ' + this.top );
     }
 
 }
 
-var hero = new Hero('khunglong.jpg', 20, 30, 200, 30);
+var hero = new Hero('khunglong.jpg', 20, 30, 200, 60);
 
 function start(){
-    if(hero.left < window.innerWidth - hero.size && hero.top < window.innerHeight - hero.size){
+    if(hero.left < window.innerWidth - hero.size && hero.left < 800){
         hero.moveRight();
+    }
+    else if (hero.top < window.innerHeight - hero.size && hero.left > 700 && hero.top < 600)
+    {
         hero.moveDown();
     }
+
     document.getElementById('game').innerHTML = hero.getHeroElement();
     setTimeout(start, 500)
 }
